41ac7a537798dfa9cd9075fefc0186e8d1c9b06c
Emyeeeel <maryamielcatado@gmail.com>
2024-10-21 12:43:06 +0800

    set up folders, simple routes for login and signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./pages/signup/signup.page').then( m => m.SignupPage)
   },
 ];
---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router'; 
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterModule],
+})
+export class HomePage {
+  constructor() {}
+}
