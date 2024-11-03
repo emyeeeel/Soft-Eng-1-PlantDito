056ad06d6ba1f01347035ac82b62398daa7cd34c
Anthony Plaza <anthonyjamesplaza@gmail.com>
2024-11-03 15:28:38 +0800

    Merged landing-page-style into signup-page

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormsModule, FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { IonLabel,IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonItem } from '@ionic/angular/standalone';
+import {  } from '@ionic/angular';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonButton, IonLabel, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  showPopup = false;
+  signupForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.signupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required]
+    });
   }
 
+  showSignupPopup() {
+    this.showPopup = true;
+  }
+
+  hidePopup() {
+    this.showPopup = false;
+  }
+
+  continueSignup() {
+    if (this.signupForm.valid) {
+      console.log('Signup form submitted:', this.signupForm.value);
+      // Add logic to proceed with signup process
+      this.hidePopup();
+    }
+  }
 }
